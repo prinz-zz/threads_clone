475cb46f353243c6f799667b2f2662f71f8752a1
Prinz Adamz <prinzadamz@gmail.com>
2023-11-27 14:08:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -42,9 +42,10 @@ cloudinary.config({
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes) 
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname, './frontend/dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+         
 })
 
 
