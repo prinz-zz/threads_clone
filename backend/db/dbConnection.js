0cf0b96567e8e5811013cd1859e8c8be1f0797ed
Prinz Adamz <prinzadamz@gmail.com>
2023-11-27 12:42:28 +0530

    Update dbConnection.js

--- backend/db/dbConnection.js
+++ backend/db/dbConnection.js
@@ -3,12 +3,9 @@ import asycHandler from 'express-async-handler'
 
 const dbConnection = asycHandler(async () => {
 
-    let options = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }; 
+    
 
-    const connect = await mongoose.connect(process.env.MONGO_URI, options);
+    const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log('Db connection established DB name:' , connect.connection.name);
 })
 
